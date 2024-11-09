b895781ab3c8cecb8667a14435b9d24a4ef2230c
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-11-09 13:01:16 +0700

    feat: add postgres image via docker compose

--- apps/fastify/src/index.ts
+++ apps/fastify/src/index.ts
@@ -8,6 +8,22 @@ fastify.get("/", async () => {
   return { hello: "world" };
 });
 
+fastify.post("/login", async () => {
+  return {
+    success: true,
+    message: "Success",
+    data: {
+      access_token: "some-access-token"
+    }
+  };
+});
+
+fastify.post("/register", async () => {
+  return {
+    message: "success"
+  };
+});
+
 const start = async () => {
   try {
     await fastify.listen({ port: 5000 });
